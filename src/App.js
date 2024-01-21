b0292c61b861ee579f09baa27a52305196c4ab3d
Thiago <martinsmthiago@gmail.com>
2024-01-21 17:29:09 -0300

    add data fetch cleaning

--- src/App.js
+++ src/App.js
@@ -79,12 +79,13 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError('');
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`, { signal: controller.signal }
           );
 
           if (!res.ok)
@@ -94,9 +95,13 @@ export default function App() {
           if (data.Response === 'False') throw new Error('Movie not found');
 
           setMovies(data.Search);
+          setError("");
         } catch (err) {
           console.log(err.message);
-          setError(err.message);
+
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -108,6 +113,10 @@ export default function App() {
         return;
       }
       fetchMovies();
+
+      return function() {
+        controller.abort();
+      }
     },
     [query]
   );
