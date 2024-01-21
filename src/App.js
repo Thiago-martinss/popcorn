1f89ca8c6709e40c2a7455cde020a612b9c39dfb
Thiago <martinsmthiago@gmail.com>
2024-01-21 16:59:00 -0300

    add clean up effect for movie title

--- src/App.js
+++ src/App.js
@@ -331,7 +331,11 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
 
   useEffect(function() {
     if (!title) return;
-    document.title = `Movie | ${title}`
+    document.title = `Movie | ${title}`;
+
+    return function() {
+      document.title = "usePopcorn";
+    }
   }, [title])
 
   return (
