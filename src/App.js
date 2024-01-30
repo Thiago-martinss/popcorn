da3f25898c329ac8fae6e5eff5caf7ff41c7a36e
Thiago <martinsmthiago@gmail.com>
2024-01-30 17:02:28 -0300

    add rating count effect

--- src/App.js
+++ src/App.js
@@ -173,7 +173,7 @@ function Search({ query, setQuery }) {
   document.addEventListener('keydown', callback)
   return () => document.addEventListener("keydown",
   callback);
-}, [])
+}, [setQuery])
 
 
   return (
@@ -282,6 +282,14 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
   const [isLoading, setIsLoading] = useState(false);
   const [userRating, setUserRating] = useState("");
 
+  const countRef = useRef(0);
+
+  useEffect(function (){
+     if (userRating) countRef.current = countRef.current++
+    }, 
+    [userRating]
+  );
+
   const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
   const watchedUserRating = watched.find(
     (movie) => movie.imdbID === selectedId
@@ -310,6 +318,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       imdbRating: Number(imdbRating),
       runtime: Number(runtime.split(" ").at(0)),
       userRating,
+      countRatingDecisions: countRef.current,
     };
     onAddWatched(newWatchedMovie);
     onCloseMovie();
