c449d731bc3b4724cf91d45d15615db61cfa2c7f
Thiago <martinsmthiago@gmail.com>
2024-03-31 18:12:59 -0300

    add useKey custom hook

--- src/useKey.js
+++ src/useKey.js
@@ -1,17 +1,20 @@
+import { useEffect } from "react";
 
-
-
-
-useEffect(function () {
-    function callback(e) {
-      if (e.code === "Escape") {
-        onCloseMovie();
+export function useKey(key, action) {
+  useEffect(
+    function () {
+      function callback(e) {
+        if (e.code.toLowerCase() === key.toLowerCase()) {
+          action();
+        }
       }
-    }
-    document.addEventListener("keydown", callback);
 
-    return function () {
-      document.removeEventListener("keydown", callback);
-    };
-  }, [onCloseMovie]
+      document.addEventListener("keydown", callback);
+
+      return function () {
+        document.removeEventListener("keydown", callback);
+      };
+    },
+    [action, key]
   );
+}
